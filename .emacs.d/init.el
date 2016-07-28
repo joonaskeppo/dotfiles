266d4d91967a8a6e523e7402be59261ccad8ee67
Joonas Keppo <joonas.s.keppo@student.jyu.fi>
2016-07-28 12:31:42 +0300

    Emacs: Add better-defaults package, fullscreening, and a few other minor additions

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,5 +1,5 @@
 (require 'package)
-(setq package-list '(evil))
+(setq package-list '(evil better-defaults))
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 (package-initialize)
 (unless package-archive-contents
@@ -14,8 +14,27 @@
 (require 'evil)
 (evil-mode 1)
 
-(menu-bar-mode -1)
-(toggle-scroll-bar -1)
-(tool-bar-mode -1)
-
+;; Start at *scratch* buffer
 (setq inhibit-startup-screen t)
+;; Global line numbers
+(global-linum-mode t)
+
+;; Full screening (adapted from http://emacswiki.org/emacs/FullScreen)
+(defun fullscreen-view ()
+  (interactive)
+  (set-frame-parameter nil 'fullscreen 'fullboth)) ;this makes the frame go fullscreen
+(defun windowed-view ()
+  (interactive)
+  (set-frame-parameter nil 'width 90)
+  (set-frame-parameter nil 'fullscreen 'fullheight))
+(defun toggle-fullscreen ()
+  (interactive)
+  (if (eq (frame-parameter nil 'fullscreen) 'fullboth)  ;tests if already fullscreened
+      (windowed-view)
+    (fullscreen-view)))
+(global-set-key (kbd "<f11>") 'toggle-fullscreen)
+
+;; Tabs -> 2 spaces
+(setq tab-width 2)
+(defvaralias 'c-basic-offset 'tab-width)
+
