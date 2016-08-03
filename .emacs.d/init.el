fadc726a8bcd308566d5e600d43d70349bca8152
Joonas Keppo <joonaskeppo@gmail.com>
2016-08-03 14:04:35 +0300

    Update init.el and add evil-escape package

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,5 +1,5 @@
 (require 'package)
-(setq package-list '(evil better-defaults))
+(setq package-list '(evil evil-escape better-defaults))
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 (package-initialize)
 (unless package-archive-contents
@@ -11,30 +11,53 @@
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
 (load-theme 'gruvbox t)
 
+;; EVIL MODE
+;; ~~~~~~~~~
+
 (require 'evil)
 (evil-mode 1)
+(setq evil-move-cursor-back nil)
 
-;; Start at *scratch* buffer
+(require 'evil-escape)
+(global-set-key (kbd "C-g") 'evil-escape)
+
+;; ~~~~~~~~~
+
+(add-hook 'before-save-hook
+          'delete-trailing-whitespace)
+
 (setq inhibit-startup-screen t)
-;; Global line numbers
 (global-linum-mode t)
+(cond
+ ((find-font (font-spec :name "Monoid"))
+  (set-face-attribute 'default nil
+                        :family "Monoid"
+                        :height 90
+                        :weight 'normal
+                        :width 'normal))
+ ((find-font (font-spec :name "Ubuntu Mono"))
+  (set-face-attribute 'default nil
+                        :family "Ubuntu Mono"
+                        :height 90
+                        :weight 'normal
+                        :width 'normal)))
+
+;; Tabs -> 2 spaces (+ manual tabbing)
+(setq c-basic-indent 2)
+(setq tab-width 2)
+(setq indent-tabs-mode nil)
 
 ;; Full screening (adapted from http://emacswiki.org/emacs/FullScreen)
 (defun fullscreen-view ()
   (interactive)
-  (set-frame-parameter nil 'fullscreen 'fullboth)) ;this makes the frame go fullscreen
+  (set-frame-parameter nil 'fullscreen 'fullboth))
 (defun windowed-view ()
   (interactive)
   (set-frame-parameter nil 'width 90)
   (set-frame-parameter nil 'fullscreen 'fullheight))
 (defun toggle-fullscreen ()
   (interactive)
-  (if (eq (frame-parameter nil 'fullscreen) 'fullboth)  ;tests if already fullscreened
+  (if (eq (frame-parameter nil 'fullscreen) 'fullboth)
       (windowed-view)
     (fullscreen-view)))
 (global-set-key (kbd "<f11>") 'toggle-fullscreen)
-
-;; Tabs -> 2 spaces
-(setq tab-width 2)
-(defvaralias 'c-basic-offset 'tab-width)
-
