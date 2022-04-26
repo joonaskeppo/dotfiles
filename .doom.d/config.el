78660ac2a216a66426162b5cfe526b612e1cfb69
Joonas Keppo <joonaskeppo@gmail.com>
2022-04-26 20:27:40 +0300

    Update Emacs configs
    This updates a lot of Org and Clojure stuff, primarily.
    For instance, an initial org-roam workflow is added.

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -1,64 +1,19 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
-;; Place your private configuration here! Remember, you do not need to run 'doom
-;; sync' after modifying this file!
+;; --- base configurations ---
 
-
-;; Some functionality uses this to identify you, e.g. GPG configuration, email
-;; clients, file templates and snippets.
 (setq user-full-name "Joonas Keppo"
       user-mail-address "joonaskeppo@gmail.com")
 
-
-;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
-;; are the three important ones:
-;;
-;; + `doom-font'
-;; + `doom-variable-pitch-font'
-;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
-;;   presentations or streaming.
-;;
-;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
-;; font string. You generally only need these two:
 (setq doom-font (font-spec :family "Fira Code" :style "Retina" :size 15)
       doom-variable-pitch-font (font-spec :family "Menlo" :size 13))
 
-;; There are two ways to load a theme. Both assume the theme is installed and
-;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. This is the default:
 (setq doom-theme 'doom-nord)
-
-;; If you use `org' and don't want your org files in the default location below,
-;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/org/")
-(setq org-roam-directory "~/org/roam/")
-
-;; This determines the style of line numbers in effect. If set to `nil', line
-;; numbers are disabled. For relative line numbers, set this to `relative'.
-(setq display-line-numbers-type 'relative)
-
-;; Here are some additional functions/macros that could help you configure Doom:
-;;
-;; - `load!' for loading external *.el files relative to this one
-;; - `use-package!' for configuring packages
-;; - `after!' for running code after a package has loaded
-;; - `add-load-path!' for adding directories to the `load-path', relative to
-;;   this file. Emacs searches the `load-path' when you load packages with
-;;   `require' or `use-package'.
-;; - `map!' for binding new keys
-;;
-;; To get information about any of these functions/macros, move the cursor over
-;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
-;; This will open documentation for it, including demos of how they are used.
-;;
-;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
-;; they are implemented.
-
-;; --- basic stuff ---
 
 ;; start with maximized window
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
 
+(setq display-line-numbers-type 'relative)
 (setq-default line-spacing 0.2)
 
 (setq doom-localleader-key ",")
@@ -70,10 +25,13 @@
 (setq +format-on-save-enabled-modes
       '(not web-mode))
 
-;; --- structural editing ---
+;; --- editing ---
 
 (add-hook! 'smartparens-enabled-hook #'evil-cleverparens-mode)
 (setq evil-move-beyond-eol t) ;; https://github.com/luxbock/evil-cleverparens/issues/29
+
+(require 'evil-multiedit)
+;(evil-multiedit-default-keybindings)
 
 ;; --- treemacs ---
 
@@ -82,17 +40,168 @@
 ;; --- org ---
 
 (require 'org-habit)
+(require 'ox-publish)
+
+(setq org-directory "~/org/"
+      org-roam-directory "~/org/roam/"
+      org-publish-project-alist
+      '(("org-notes"
+         :base-directory "~/org/roam/"
+         :base-extension "org"
+         :publishing-directory "~/org/roam/build/"
+         :recursive t
+         :publishing-function org-html-publish-to-html)))
+
+(defun publish-notes ()
+  "Publish all org-roam notes"
+  (interactive)
+  (shell-command "rm ~/org/roam/build/**/*.html")
+  (org-publish "org-notes" t))
 
 (setq org-babel-clojure-backend 'cider)
 
-;; (use-package! org-super-agenda
-;;   :after  org-agenda
-;;   :init
-;;   (setq org-super-agenda-groups '((:name "asdasdssdsda" :habit t)
-;;                                   (:name "Important" :priority "A")))
-;;   :config
-;;   (org-super-agenda-mode))
+;; ** org-roam workflow stuff **
+;;
+;; adapted from org-roam creator's excellent article on his personal org-roam workflow
+;; (mimicking Luhmann's original workflow):
+;; https://jethrokuan.github.io/org-roam-guide/
+;;
+;; the specific workflow combines Jethro's workflow with David Kadavy's,
+;; as detailed in his book, Digital Zettelkasten.
+;;
+;; the org-roam directory structure will look like this:
+;;
+;; .
+;; ├── articles
+;; ├── permanent
+;; ├── literature
+;; ├── raw
+;; ├── maybe
+;; ├── inbox.org
+;; └── refs.bib
+;;
+;; where:
+;; - each folder is a "slipbox",
+;; - `inbox.org' is an "inbox" of as-yet unprocessed thoughts and ideas, and
+;; - `refs.bib' contains all bibtex references, exported and sync'd automagically using Better BibTeX + Zotero.
+
+(setq org-roam-capture-templates
+      '(;; permanent notes -- notes by yours truly, in my own words, synthesizing previous notes on a single topic
+        ("p" "permanent" plain "%?"
+         :if-new (file+head "permanent/${slug}.org"
+                            "#+title: ${title}\n#+filetags: :permanent:\n")
+         :immediate-finish t
+         :unnarrowed t)
+        ;; literature notes -- notes from various media in my own words
+        ("l" "literature" plain "%?"
+         :if-new
+         (file+head "literature/${title}.org"
+                    "#+title: ${title}\n#+filetags: :literature:\n")
+         :immediate-finish t
+         :unnarrowed t)
+        ;; articles -- articles generated principally from permanent notes
+        ("a" "article" plain "%?"
+         :if-new
+         (file+head "articles/${title}.org"
+                    "#+title: ${title}\n#+filetags: :article:\n")
+         :immediate-finish t
+         :unnarrowed t)
+        ;; raw notes -- exported highlights from various media; for those cases where literature notes don't contain exact quotes or highlights
+        ("r" "raw" plain "%?"
+         :if-new
+         (file+head "raw/${title}.org"
+                    "#+title: ${title}\n#+filetags: :raw:\n")
+         :immediate-finish t
+         :unnarrowed t)
+        ;; "someday/maybe" -- things that are interesting, but irrelevant to current areas of focus
+        ("m" "maybe" plain "%?"
+         :if-new
+         (file+head "maybe/${title}.org"
+                    "#+title: ${title}\n#+filetags: :maybe:\n")
+         :immediate-finish t
+         :unnarrowed t)))
+
+(cl-defmethod org-roam-node-type ((node org-roam-node))
+  "Return the TYPE of NODE."
+  (condition-case nil
+      (file-name-nondirectory
+       (directory-file-name
+        (file-name-directory
+         (file-relative-name (org-roam-node-file node) org-roam-directory))))
+    (error "")))
+
+(setq org-roam-node-display-template
+      (concat "${type:15} ${title:*} "
+              (propertize "${tags:10}" 'face 'org-tag)))
+
+(setq org-capture-templates
+      (add-to-list 'org-capture-templates
+                   `("s" "Slipbox" entry (file ,(concat org-roam-directory "inbox.org"))
+                     "* %?\n")))
+
+(defun org-roam-capture-inbox ()
+  (interactive)
+  (org-capture nil "s"))
+
+;; creating literature notes
+(defun org-roam-node-from-cite (keys-entries)
+    (interactive (list (citar-select-ref :multiple nil :rebuild-cache t)))
+    (let ((title (citar--format-entry-no-widths (cdr keys-entries)
+                                                "${author editor} :: ${title}")))
+      (org-roam-capture- :templates
+                         '(("l" "literature" plain "%?" :if-new
+                            (file+head "literature/${citekey}.org"
+                                       ":PROPERTIES:
+:ROAM_REFS: [cite:@${citekey}]
+:END:
+#+title: ${title}\n#+filetags: :literature:\n")
+                            :immediate-finish t
+                            :unnarrowed t))
+                         :info (list :citekey (car keys-entries))
+                         :node (org-roam-node-create :title title)
+                         :props '(:finalize find-file))))
+
+(defun org-roam-tag-node-as-draft ()
+  (org-roam-tag-add '("draft")))
+
+;; make sure all new zettels are marked with tag `draft'
+(add-hook 'org-roam-capture-new-node-hook #'org-roam-tag-node-as-draft)
+
+;; publishing org-roam notes into HTML
+(require 'ox-publish)
+
+
+;; bibtex completion via `biblio' module:
+;; https://github.com/hlissner/doom-emacs/tree/master/modules/tools/biblio#path-configuration
+(after! org-cite
+  (setq! bibtex-completion-bibliography
+         (list (concat org-roam-directory "refs.org"))))
 
 ;; --- misc ---
 
 (setq +doom-dashboard-banner-file (expand-file-name "emacs-e.svg" doom-private-dir))
+
+;; --- clojure ---
+
+;; Leverage an existing cider nrepl connection to evaluate portal.api functions
+;; and map them to convenient key bindings.
+
+(defun portal.api/open ()
+  (interactive)
+  (cider-nrepl-sync-request:eval
+   "(require 'portal.api) (portal.api/tap) (portal.api/open)"))
+
+(defun portal.api/clear ()
+  (interactive)
+  (cider-nrepl-sync-request:eval "(portal.api/clear)"))
+
+(defun portal.api/close ()
+  (interactive)
+  (cider-nrepl-sync-request:eval "(portal.api/close)"))
+
+;; Example key mappings for doom emacs
+(map! :map clojure-mode-map
+      ;; cmd  + o
+      :n "s-o" #'portal.api/open
+      ;; ctrl + l
+      :n "C-l" #'portal.api/clear)
