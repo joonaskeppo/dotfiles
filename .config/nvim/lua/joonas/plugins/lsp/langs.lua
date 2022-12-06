079d2adb451cc3c88e6cf0c4016812532f58a9fd
Joonas Keppo <joonaskeppo@gmail.com>
2022-12-06 13:15:20 +0200

    Fix formatting in nvim files

--- .config/nvim/lua/joonas/plugins/lsp/langs.lua
+++ .config/nvim/lua/joonas/plugins/lsp/langs.lua
@@ -1,27 +1,27 @@
-local handlers = require 'joonas.plugins.lsp.handlers'
+local handlers = require("joonas.plugins.lsp.handlers")
 
 local base_opts = {
-    on_attach = handlers.on_attach,
-    capabilities = handlers.capabilities,
+	on_attach = handlers.on_attach,
+	capabilities = handlers.capabilities,
 }
 
 local servers = {
-    sumneko_lua = require('joonas.plugins.lsp.settings.sumneko_lua'),
-    clangd = require('joonas.plugins.lsp.settings.clangd'),
-    clojure_lsp = require('joonas.plugins.lsp.settings.clojure_lsp'),
+	sumneko_lua = require("joonas.plugins.lsp.settings.sumneko_lua"),
+	clangd = require("joonas.plugins.lsp.settings.clangd"),
+	clojure_lsp = require("joonas.plugins.lsp.settings.clojure_lsp"),
 }
 
 local server_keyset = {}
 for key, _ in pairs(servers) do
-    table.insert(server_keyset, key)
+	table.insert(server_keyset, key)
 end
 
-require('mason').setup()
-require('mason-lspconfig').setup({
-    ensure_installed = server_keyset
+require("mason").setup()
+require("mason-lspconfig").setup({
+	ensure_installed = server_keyset,
 })
 
 for lang, lang_opts in pairs(servers) do
-    local server_opts = vim.tbl_deep_extend('force', lang_opts, base_opts)
-    require('lspconfig')[lang].setup(server_opts)
+	local server_opts = vim.tbl_deep_extend("force", lang_opts, base_opts)
+	require("lspconfig")[lang].setup(server_opts)
 end
