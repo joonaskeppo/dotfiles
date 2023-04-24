c4ddd9a6ef4d048168a06d52c7b9ccc859fa248a
Joonas Keppo <joonas.keppo@solita.fi>
2023-04-24 08:33:20 +0300

    Update lein profiles

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,2 +1,8 @@
-{:user {:dependencies [[djblue/portal "0.35.1"]]
-        :plugins      [[cider/cider-nrepl "0.29.0"]]}}
+{:user {:dependencies [[djblue/portal "0.35.1"]
+                       [org.clojure/tools.namespace "1.3.0"]]
+        :plugins      [[cider/cider-nrepl "0.28.4"]
+                       [refactor-nrepl "3.5.2"]
+                       [lein-figwheel "0.5.20"]]
+        :middleware   [cider-nrepl.plugin/middleware
+                       refactor-nrepl.plugin/middleware]}}
+
